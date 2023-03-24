92ebe716d796af058f946709370360ebd8c8c660
nadia <reach.nadia.saria@gmail.com>
2023-03-23 20:11:57 -0700

    first official commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+"""
+def game(): 
+  game_board = (build_deck())
+  player_hand = deal_hand(game_board)
+  player_score = score_hand(player_hand)
+  while player_score < 21:
+     print_hand(player_hand)
+     ask_to_hit = bool(get_input("hit? "))
+     if ask_to_hit:
+        player_hand
+  print()
+"""
